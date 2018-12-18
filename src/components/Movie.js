c49ea5d77dd1537ae74c31910d003852852c8d72
Pushpa Agr <pushpaagr108@gmail.com>
2018-12-18 14:09:13 -0800

    added minor css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import "./Movie.css";
+
 
 const Movie = (props) => {
 
 
 
   return (
-    <div>
-    <p>{props.title}</p>
+    <div className="library-container">
+      <div className="item-list_container">
+        <div className="item_details">
+        <img src={props.imageUrl}/>
+          <h2>{props.title}</h2>
+          <p>{props.releaseDate}</p>
+        </div>
+        <button className="item_button">Select for Rental</button>
+      </div>
     </div>
   );
 }
