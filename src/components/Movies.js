bf5f6bf64f7c7995657cf9fca816fb8132579f7a
Katrina Aganon <katrina.aganon@gmail.com>
2018-12-18 14:11:03 -0800

    movie response data working with query

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchForm from './SearchForm'
+import axios from 'axios';
+
 
 class Movies extends Component {
   constructor(props) {
@@ -6,18 +10,50 @@ class Movies extends Component {
 
     this.state = {
       movies: [],
-    }
-  }
+      query: '',
+    };
+  };
 
+
+  searchMovie = (queryInput) => {
+    console.log(queryInput.query);
+    const url = `http://localhost:3000/movies?query=${queryInput.query}`
+    console.log(url);
+    axios.get(url)
+    .then((response) => {
+      console.log(response.data);
+      this.setState({
+        movies: response.data,
+      });
+    })
+    .catch((error) => {
+      this.setState({
+        error: error,
+      });
+    });
+    console.log(this.state.movies);
+  };
+
+  const moviesList = this.state.movies.map((movie, i) => {
+    return
+  })
 
   render() {
+
+
     return(
-      <h1>TEST in Movies </h1>
-    )
-  }
+      <div>
+        <h1>TEST in Movies </h1>
+        <SearchForm searchQueryCallback={this.searchMovie}/>
+      </div>
+
+    );
+
+
+  };
 
 
 
-}
+};
 
 export default Movies
---
+++ src/components/SearchForm.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: '',
+    };
+  };
+
+
+  onInputChange = (event) => {
+    const newState = {};
+    newState[event.target.name] = event.target.value;
+
+    this.setState(newState)
+    console.log(this.state.query);
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { query } = this.state;
+
+    this.props.searchQueryCallback(this.state);
+
+    this.setState({
+      query: '',
+    });
+
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <input name="query" value={this.state.query} onChange={this.onInputChange} placeholder="Enter a movie title" type="text" className="form-input"/>
+      </form>
+
+    )
+  }
+
+
+
+
+
+
+
+}
+
+SearchForm.propTypes = {
+  searchQueryCallback: PropTypes.func.isRequired,
+};
+
+
+export default SearchForm;
