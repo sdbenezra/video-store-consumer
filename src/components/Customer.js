6d3b7dc0a42da2f191d5f23a3481a97aaee3b5b2
Sigrid Benezra <sdbenezra@gmail.com>
2018-12-18 19:19:17 -0800

    Tweak css on customer list

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -5,14 +5,17 @@ import "./Customer.css"
 const Customer = (props) => {
   const { name, movies_checked_out_count, customerCallback} = props;
   return (
-    <div className="item">
-      <div className="item_details">
-        <h2>{props.name}</h2>
-        <p></p>
-        <p>{`${props.movies_checked_out_count} movies checked out`}</p>
-        <button onClick={() => customerCallback()}>Select for Rental</button>
+    <div className="item-list_container">
+      <div className="customer item">
+        <div className="item__details">
+          <h2>{props.name}</h2>
+          <p></p>
+          <p>{`${props.movies_checked_out_count} movies checked out`}</p>
+        </div>
+          <button className="item__button" onClick={() => customerCallback()}>Select for Rental</button>
       </div>
     </div>
+
   )
 }
 
